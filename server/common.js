1b9b477f3215fa0ad53088fa31b5e9cecaeccedc
Daekyung Kim <timkim0713@gmail.com>
2022-04-22 19:10:42 -0400

    settings update

--- server/common.js
+++ server/common.js
@@ -1,5 +1,5 @@
-exports.IS_PRODUCTION_MODE = true;
-exports.PROD_IP = "kylerim.cse356.compas.cs.stonybrook.edu"
-exports.LOCAL_IP = "localhost"
+exports.IS_PRODUCTION_MODE = false;
+exports.PROD_IP = "kylerim.cse356.compas.cs.stonybrook.edu";
+exports.LOCAL_IP = "localhost";
 exports.GROUP_ID = "61f9cf4e4261123151824fdc";
-exports.websocketServer = 'ws://localhost:5555';
+exports.websocketServer = "ws://localhost:5555";

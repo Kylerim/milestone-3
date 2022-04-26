e86a34203863a5c84b15e1b80b32a1eb679dab97
Daekyung Kim <timkim0713@gmail.com>
2022-04-25 21:00:49 -0400

    elastic search server port

--- server/common.js
+++ server/common.js
@@ -3,3 +3,4 @@ exports.PROD_IP = "kylerim.cse356.compas.cs.stonybrook.edu";
 exports.LOCAL_IP = "localhost";
 exports.GROUP_ID = "61f9cf4e4261123151824fdc";
 exports.websocketServer = "ws://localhost:5555";
+exports.ELASTIC_PORT = 6001;

e08c52679c5d6f84a3ceeca4865a476adaa08a01
Kyuri-Kyeong <vinca1022@gmail.com>
2022-04-28 11:59:38 -0400

    log

--- server/server.js
+++ server/server.js
@@ -26,10 +26,10 @@ const {
     websocketServer,
 } = require("./common.js");
 
-// if (IS_PRODUCTION_MODE) {
-//     console = console || {};
-//     console.log = function () {};
-// }
+if (IS_PRODUCTION_MODE) {
+    console = console || {};
+    console.log = function () {};
+}
 const {
     adduser,
     login,

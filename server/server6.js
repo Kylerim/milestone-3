16312d3c903cac1df7cd70f7cd89752021ac8e84
Tyler Htet Naing Phyo <42888726+TylerHNP@users.noreply.github.com>
2022-04-28 08:31:24 -0400

    server 6 with more emitters

--- server/server6.js
+++ server/server6.js
@@ -1,6 +1,6 @@
 /* eslint no-console: ["error", { allow: ["warn", "error"] }] */
 
-// emitter.setMaxListeners(0);
+emitter.setMaxListeners(0);
 
 const WebSocket = require("ws");
 const express = require("express");

348678c31ccd41ec908057835c422e6d158ad3e4
Tyler Htet Naing Phyo <42888726+TylerHNP@users.noreply.github.com>
2022-04-30 02:20:26 -0400

    removing \n

--- server/controllers/elastic.js
+++ server/controllers/elastic.js
@@ -18,7 +18,8 @@ const queueCallback = async function ({ id, delta }, completed) {
     let html = converter.convert();
     const content = convert(html, {
         wordwrap: null,
-    });
+    }).replace(/(\r\n|\n|\r)/gm, "");
+
     // const content = toPlaintext(delta);
     console.log("Updating content", content);
     // const result = await client.update({
